83da1ebf1b3a5f000475996b67b6c9f8bef670a9
Dávid Ondruš <>
2025-06-18 22:01:12 +0200

    Remove logs service unused methods

--- feathers-logs-service/src/services/logs/logs.js
+++ feathers-logs-service/src/services/logs/logs.js
@@ -1,34 +1,26 @@
-// For more information about this file see https://dove.feathersjs.com/guides/cli/service.html
-
 import { hooks as schemaHooks } from '@feathersjs/schema'
 import {
-  logsDataValidator,
-  logsPatchValidator,
+  logsCreateValidator,
   logsQueryValidator,
   logsResolver,
   logsExternalResolver,
   logsDataResolver,
-  logsPatchResolver,
   logsQueryResolver
 } from './logs.schema.js'
 import { LogsService, getOptions } from './logs.class.js'
 
 export const logsPath = 'logs'
-export const logsMethods = ['find', 'get', 'create', 'patch', 'remove']
+export const logsMethods = ['find', 'create']
 
 export * from './logs.class.js'
 export * from './logs.schema.js'
 
-// A configure function that registers the service and its hooks via `app.configure`
 export const logs = app => {
-  // Register our service on the Feathers application
   app.use(logsPath, new LogsService(getOptions(app)), {
-    // A list of all methods this service exposes externally
     methods: logsMethods,
-    // You can add additional custom events to be sent to clients here
     events: []
   })
-  // Initialize hooks
+
   app.service(logsPath).hooks({
     around: {
       all: [schemaHooks.resolveExternal(logsExternalResolver), schemaHooks.resolveResult(logsResolver)]
@@ -36,10 +28,7 @@ export const logs = app => {
     before: {
       all: [schemaHooks.validateQuery(logsQueryValidator), schemaHooks.resolveQuery(logsQueryResolver)],
       find: [],
-      get: [],
-      create: [schemaHooks.validateData(logsDataValidator), schemaHooks.resolveData(logsDataResolver)],
-      patch: [schemaHooks.validateData(logsPatchValidator), schemaHooks.resolveData(logsPatchResolver)],
-      remove: []
+      create: [schemaHooks.validateData(logsCreateValidator), schemaHooks.resolveData(logsDataResolver)],
     },
     after: {
       all: []
