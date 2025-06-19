af74240d5857b1e29af9fa445f07bfa29f87ebab
Dávid Ondruš <>
2025-06-19 09:52:15 +0200

    Edit logs schema

--- feathers-logs-service/src/services/logs/logs.schema.js
+++ feathers-logs-service/src/services/logs/logs.schema.js
@@ -1,51 +1,31 @@
-// For more information about this file see https://dove.feathersjs.com/guides/cli/service.schemas.html
 import { resolve, getValidator, querySyntax } from '@feathersjs/schema'
 import { ObjectIdSchema } from '@feathersjs/schema'
 import { dataValidator, queryValidator } from '../../validators.js'
 
-// Main data model schema
 export const logsSchema = {
   $id: 'Logs',
   type: 'object',
   additionalProperties: false,
-  required: ['_id', 'text'],
+  required: ['text', 'level', 'time', 'type'],
   properties: {
     _id: ObjectIdSchema(),
-    text: { type: 'string' }
+    text: { type: 'string' },
+    level: {
+      type: 'number',
+      minimum: 1,
+      maximum: 9,
+    },
+    time: { type: 'string' },
+    type: { enum: ['system', 'user'] },
   }
 }
-export const logsValidator = getValidator(logsSchema, dataValidator)
 export const logsResolver = resolve({})
 
 export const logsExternalResolver = resolve({})
 
-// Schema for creating new data
-export const logsDataSchema = {
-  $id: 'LogsData',
-  type: 'object',
-  additionalProperties: false,
-  required: ['text'],
-  properties: {
-    ...logsSchema.properties
-  }
-}
-export const logsDataValidator = getValidator(logsDataSchema, dataValidator)
+export const logsCreateValidator = getValidator(logsSchema, dataValidator)
 export const logsDataResolver = resolve({})
 
-// Schema for updating existing data
-export const logsPatchSchema = {
-  $id: 'LogsPatch',
-  type: 'object',
-  additionalProperties: false,
-  required: [],
-  properties: {
-    ...logsSchema.properties
-  }
-}
-export const logsPatchValidator = getValidator(logsPatchSchema, dataValidator)
-export const logsPatchResolver = resolve({})
-
-// Schema for allowed query properties
 export const logsQuerySchema = {
   $id: 'LogsQuery',
   type: 'object',
