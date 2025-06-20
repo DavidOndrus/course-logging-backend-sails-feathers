4a2f5310024b183d8edbb804b2b87346ef97c601
Dávid Ondruš <>
2025-06-21 00:11:15 +0200

    Edit post logs request

--- sails-api/api/controllers/logs/create.js
+++ sails-api/api/controllers/logs/create.js
@@ -1,4 +1,6 @@
+const axios = require('axios');
+
 module.exports = async function create(req, res) {
-  sails.log.debug('TODO: implement');
-  return res.status(501).send();
+  const response = await axios.post(`http://feathers-logs-service:8081${req.path}`, req.body);
+  return res.status(response.status).send(response.data);
 };
