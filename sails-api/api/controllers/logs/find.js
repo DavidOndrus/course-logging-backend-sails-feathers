c71687c9824ddf957f5aa0e1a4519a1d980fa02b
Dávid Ondruš <>
2025-06-21 10:20:48 +0200

    Add sails logs endpoint input validation

--- sails-api/api/controllers/logs/find.js
+++ sails-api/api/controllers/logs/find.js
@@ -1,6 +1,8 @@
 const forwardRequest = require('../../utils/forward-request.js');
 
 module.exports = async function find(req, res) {
+  req.query.type = 'system';
+
   const response = await forwardRequest(req);
   return res.status(response.status).send(response.data);
 };
