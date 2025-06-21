55550f3cb1c3fd8397ab923ca811b37adf309ac8
Dávid Ondruš <>
2025-06-21 08:01:56 +0200

    Edit get logs request

--- sails-api/api/controllers/logs/find.js
+++ sails-api/api/controllers/logs/find.js
@@ -1,4 +1,8 @@
+const axios = require('axios');
+
 module.exports = async function find(req, res) {
-  sails.log.debug('TODO: implement');
-  return res.status(501).send();
+  const response = await axios.get(`http://feathers-logs-service:8081${req.path}`, {
+    params: req.query,
+  });
+  return res.status(response.status).send(response.data);
 };
